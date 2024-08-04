4a1046f196ab094aef700f147289c53f79b80dc4
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-08-03 20:45:56 -0500

    problema de sintaxis de description resuelto, template home lista, en models y views

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,6 @@ from django.db.models.fields.files import ImageField
 
 class Project(models.Model):
     title = CharField(max_length=100)
-    descripiton = CharField(max_length=250)
+    description = CharField(max_length=250)
     image = ImageField(upload_to='portfolio/images/')
     url = URLField(blank=True)
